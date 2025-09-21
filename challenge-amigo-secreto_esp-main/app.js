835f4ac7e80cbfb6bff2e30052586c17b48ba520
JazGarvez <125822598+JazGarvez@users.noreply.github.com>
2025-09-21 13:32:03 -0600

    Add files via upload

---
+++ challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,8 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombresAmigos = [];
+let nombreElegido = 0;
+
+
+function añadir (){
+    
+}
